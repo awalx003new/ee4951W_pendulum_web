cdabeb51ce90db123dccfd1e5476569b76089594
awalx003new <rize.kat11@gmail.com>
2021-10-14 03:28:29 -0500

    Deleted files that get created when you run the server

--- app/app/FileProcessing/Uploads/initialize_system.py
+++
@@ -1,9 +0,0 @@
-# This file should be run on system startup. It will initialize the linear position to the center so that all tests originate from a proper position.
-# The center is found by using the hardware limit switches
-from system import System
-import RPi.GPIO as GPIO
-        
-# Main program
-sys = System()
-sys.initialize()
-GPIO.cleanup()
