c908902d2338232547f1fa45bd9b01bb878953e4
awalx003new <rize.kat11@gmail.com>
2021-10-17 08:57:36 -0500

    Added comments to code

--- app/app/views.py
+++ app/app/views.py
@@ -1,37 +1,36 @@
 #---------------------------------------------------------imports---------------------------------------------------------------#
 from flask import Flask, jsonify, request, render_template, make_response, redirect, url_for, send_from_directory, flash, Request
-import os # Use for path navigation
-import sys # Use for path navigation
-import time # Use for sleep()
+# Use for operating system functionalities
+import os
+# Use for generating random secret key
+import secrets
+# Use for running a python script
 import subprocess
 from werkzeug.utils import secure_filename
 
+# The 'static_folder' argument is the folder with static files that is served at
 app = Flask(__name__, static_folder='static')
-# The 'static' argument is the folder with static files that is served at
 
 # Set the secret key to some random bytes. Needs to be top secret!
-# For POST request
-app.secret_key = "ski u mah"
+# Needed for POST request
+app.secret_key = secrets.token_hex()
 
 # Maximum file size is 16 MB
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
-# Uploads folder is in current after changing directory to ./FileProcessing/Uploads directory
-app.config['UPLOAD_FOLDER'] = "."
-
 DOWNLOAD_DIRECTORY = "./FileProcessing/Downloads"
-
-UPLOAD_DESTINATION = "./FileProcessing/Uploads"
+UPLOAD_DESTINATION = "."
 RESULTS_DESTINATION = "."
 
-ALLOWED_EXTENSIONS = {'py'}
-#PROCESSING_FILE = "upload.py"
+# Uploads folder is in current directory after changing directory to ./FileProcessing/Uploads directory
+app.config['UPLOAD_FOLDER'] = UPLOAD_DESTINATION
 
-# Create an 'Uploads' directory in a known location --> this directory is where files are saved to
-#uploads_directory = os.path.join(app.instance_path, UPLOAD_DESTINATION)
-#os.makedirs(uploads_directory, exist_ok=True)
+ALLOWED_EXTENSIONS = {'py'}
+# Name of test file
+PROCESSING_FILE = "upload.py"
 
 #---------------------------------------------------------methods------------------------------------------------------------------#
+# Only allow .py file extension
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -72,18 +71,18 @@ def upload_file():
         uploads_dir = "Uploads"
 
         absolutepath = os.path.abspath(__file__)
-        print(absolutepath)
+        #print(absolutepath)
 
         fileDirectory = os.path.dirname(absolutepath)
-        print(fileDirectory)
+        #print(fileDirectory)
 
         #Navigate to ./FileProcessing directory
         newPath1 = os.path.join(fileDirectory, 'FileProcessing')
-        print(newPath1)
+        #print(newPath1)
 
         #Navigate to ./Uploads directory
         newPath2 = os.path.join(newPath1, uploads_dir)
-        print(newPath1)
+        #print(newPath1)
 
         # Make directory called "Uploads"
         # mode: read, write, and execute permissions for user, owner, and group
@@ -93,10 +92,6 @@ def upload_file():
         # change directory to "Uploads"
         os.chdir(newPath2)
 
-        # Get current working Directory
-        #print("Current working directory is ")
-        #print(os.getcwd())
-
         # Get the file name
         nameOfFile = secure_filename(uploaded_file.filename)
 
@@ -104,7 +99,7 @@ def upload_file():
         uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], nameOfFile))
 
         # Open the ("Uploads/" conconcatenated with "filename") file for read
-        userFile = open("." + "/" + nameOfFile, "r")
+        userFile = open(UPLOAD_DESTINATION + "/" + nameOfFile, "r")
 
         fileContents = userFile.read()
 
@@ -112,7 +107,7 @@ def upload_file():
 
         # Please note that "upload.py" is the test file
         # Open the ("Uploads/" conconcatenated with "upload.py") file for read/write.  Create upload.py if it does not already exist
-        processingFile = open("." + "/" + "upload.py", "w+")
+        processingFile = open(UPLOAD_DESTINATION + "/" + PROCESSING_FILE, "w+")
 
         # write the "file_content" to the upload file object
         processingFile.write(fileContents)
@@ -149,14 +144,7 @@ def upload_file():
         # Name of file object is "results" - open it in read mode
         with open(RESULTS_DESTINATION + "/" + results_custom_filename, "r") as customResultsFile:
             resultsContents = customResultsFile.read()
-            # customResultsFile.close() automatically done with "with...as" statement
-
-        #print("Current dir = ")
-        #print(os.getcwd())
-
-        #print("this is newPath3")
-        #print(newPath3)
-
+            # "customResultsFile.close()" automatically called with "with...as" statement
     else:
         # Display this message in the website when the user has chosen a non-Python file
         flash('The file type specified is not allowed for upload.  Allowed file type is .py', "danger")
@@ -178,7 +166,7 @@ def upload_file():
     #finally:
         #GPIO.cleanup()
 
-    # -----------------Remove contents of Results folder-----------------------
+    # ---------------------------------Remove contents of Results folder------------------------------------------------------
 
     # Change back to FileProcessing folder (from current working directory) so we can properly delete Results folder later on
     os.chdir("..")
@@ -189,15 +177,15 @@ def upload_file():
     # Remove all files in Results folder
     for i in results_files_list:
         os.remove(newPath3+"/"+i)
-        print("here A")
+        #print("here A")
 
     try:
         os.rmdir(newPath3)
-        print("here B")
+        #print("here B")
     except OSError as e:
         print("Error: %s : %s" % (newPath3, e.strerror))
 
-    # -----------------Remove contents of Uploads folder-----------------------
+    # ---------------------------------Remove contents of Uploads folder------------------------------------------------------
     # Here, we are still at the FileProcessing folder
 
     # Get list of all files in Uploads folder
@@ -206,16 +194,16 @@ def upload_file():
     # Remove all files in Uploads folder
     for j in results_files_list:
         os.remove(newPath2+"/"+j)
-        print("here C")
+        #print("here C")
 
     # Delete Uploads folder
     try:
         os.rmdir(newPath2)
-        print("here D")
+        #print("here D")
     except OSError as e:
         print("Error: %s : %s" % (newPath2, e.strerror))
 
-    #-----------------------------------------------------------------
+    #--------------------------------------------------------------------------------------------------------------------------
     return redirect(url_for('results_page'))
     #return render_template("index.html")
     #return "File upload is successful!"
@@ -233,42 +221,17 @@ def download(filename):
     except FileNotFoundError:
         abort(404)
 
-# For button that redirects the user to our GitHub repository
+# For the button that redirects the user to our GitHub repository
 @app.route('/github_repo')
 def gitHub_repo():
     return redirect("https://github.com/awalx003new/ee4951W_pendulum_web")
 
-#@app.route('/test', methods=['POST'])
-#def testfn():
-    # GET request
-    #if request.method == 'GET':
-    #    message = {'greeting':'Hello from Flask!'}
-    #    return jsonify(message)  # serialize and use JSON headers
-    # POST request
-    #if request.method == 'POST':
-    #req=request.get_json()
-    #print(req)  # parse as JSON
-    #res= make_response(jsonify(req),200)
-    #return res
-    #    return 'Sucesss', 200
-
-#@app.route('/getdata/<index_no>', methods=['GET','POST'])
-#def data_get(index_no):
-
-    #if request.method == 'POST': # POST request
-        #print(request.get_text())  # parse as text
-        #return 'OK', 200
-
-    #else: # GET request
-        #return 't_in = %s ; result: %s ;'%(index_no, data[int(index_no)])
-#############################
-# Additional code goes here #
-#############################
-#########  run app  #########
-
+#--------------------------------Run the application-----------------------------------------
 if __name__ == '__main__':
     #repopulate app.routes with static IP address for controls lab
     #Use Sam's domain name (DNS) instead of local host
-    #DNS=192.168.71.241
+    #IMPORTANT: DNS=192.168.71.241
     #app.run(host="<IP_Address>", port=<port_number>, debug=True)
     app.run(debug=True)
+    # Allow operating system to listen on all public IP Addresses
+    # app.run(host='0.0.0.0')
